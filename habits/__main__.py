e9bb82a8658df16404590a9e5e7978056a39e7c9
Jay Vana <jsvana@mtu.edu>
2017-01-02 19:02:12 -0800

    Add DB wrapper

--- habits/__main__.py
+++ habits/__main__.py
@@ -2,6 +2,8 @@
 Quick tool to pull from a Trello board and text the user a reminder
 every day. Meant to be run in cron.
 """
+import argparse
+import datetime
 import random
 import sys
 
@@ -9,6 +11,7 @@ import sys
 from twilio.rest import TwilioRestClient
 from . import (
     config,
+    db,
     trello,
 )
 
@@ -25,7 +28,32 @@ def send_message(client, text):
     return message.sid
 
 
+def init_db():
+    db.Activity.create_table()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--no-text',
+        action='store_true',
+        help='Do not send a text message with the daily activity',
+    )
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+
+    init_db()
+
+    # TODO(jsvana): put into webserver for text callback
+    # db.Activity(card_id='586afda4cc4b9b4ba002c10e').save()
+    # today = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+    # for activity in db.Activity.get(create_date=today):
+        # print(activity.card_id)
+    # return False
+
     board = trello.Board.get(config.trello['board_id'])
     possible_activities = []
     for list_name in config.trello['todo_lists']:
@@ -34,15 +62,17 @@ def main():
             possible_activities.append(card.name)
 
     activity = random.choice(possible_activities)
+    message = "Today's activity: " + activity
+
+    if args.no_text:
+        print(message)
+        return True
 
     client = TwilioRestClient(
         config.twilio['account_sid'],
         config.twilio['auth_token'],
     )
-    send_message(
-        client,
-        "Today's activity: " + activity,
-    )
+    send_message(client, message)
 
     return True
 
---
+++ habits/db.py
@@ -0,0 +1,186 @@
+"""
+Contains database-backed objects with convenience methods for fetching and
+saving.
+"""
+import logging
+
+
+from inflection import tableize
+import sqlite3
+
+
+LOG = logging.getLogger(__name__)
+
+
+class Object(object):
+    """
+    Generic SQLite-backed object
+    """
+
+    # Location of backing database
+    DB_FILE = 'habits.db'
+
+    def __init__(self, *args, **kwargs):
+        # Assume field values are presented in the same order as values
+        if args:
+            assert len(args) == len(self.fields())
+            for name, value in zip(self.fields(), args):
+                setattr(self, name, value)
+
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+
+    def table(self):
+        return self.__class__.table()
+
+    @classmethod
+    def table(cls):
+        return tableize(cls.__name__)
+
+    @classmethod
+    def fields(cls):
+        """
+        Returns a dictionary of {field_name: field_type} for the SQLite table
+        structure.
+
+        Must be implemented by children.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def required_fields(cls):
+        """
+        Returns a list of required fields of an object.
+
+        Must be implemented by children.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def optional_fields(cls):
+        """
+        Returns a list of optional fields of an object.
+        """
+        return list(set(cls.fields()) - set(cls.required_fields()))
+
+    @property
+    def field_values(self):
+        """
+        Returns a dictionary of {field_name: field_value or None} for the object
+        from the database.
+        """
+        values = {}
+        for name in self.fields():
+            values[name] = getattr(self, name, None)
+        return values
+
+    @classmethod
+    def enum_field(cls, name, field_type, choices):
+        """
+        Builds the SQLite string for an enum field
+        """
+        return "{} CHECK({} IN ({}))".format(
+            field_type,
+            name,
+            ",".join(["'{}'".format(c) for c in choices])
+        )
+
+    def save(self):
+        """
+        Saves the object into the database.
+        """
+        keys, values = list(
+            zip(
+                *[(k, v) for k, v in self.field_values.items() if v is not None]
+            )
+        )
+
+        sql = "INSERT INTO `{}` ({}) VALUES ({})".format(
+            self.table(),
+            ",".join(["`{}`".format(k) for k in keys]),
+            ",".join(["?"] * len(values)),
+        )
+        LOG.debug("[INSERT] " + sql)
+        with sqlite3.connect(self.DB_FILE) as conn:
+            conn.execute(sql, values)
+
+    @classmethod
+    def create_table(cls):
+        """
+        Creates the proper table structure in the database.
+        """
+        sql = "CREATE TABLE IF NOT EXISTS `{}` ({})".format(
+            cls.table(),
+            ",".join([
+                "`{}` {}".format(k, v) for k, v in cls.fields().items()
+            ]),
+        )
+        LOG.debug("[CREATE] " + sql)
+        with sqlite3.connect(cls.DB_FILE) as conn:
+            conn.execute(sql)
+
+    @classmethod
+    def get(cls, **constraints):
+        """
+        Gets all rows in the object's table with optional constraints.
+        """
+        sql = "SELECT {} FROM `{}`".format(
+            ",".join(["`{}`".format(k) for k in cls.fields().keys()]),
+            cls.table(),
+        )
+
+        values = []
+        if constraints:
+            sql += " WHERE"
+            for key, value in constraints.items():
+                sql += " `{}` ".format(key)
+                if value is None:
+                    sql += "IS NULL"
+                else:
+                    sql += "= ?"
+                    values.append(value)
+
+        LOG.debug("[SELECT] " + sql)
+
+        with sqlite3.connect(cls.DB_FILE) as conn:
+            cur = conn.cursor()
+            cur.execute(sql, values)
+            for row in cur.fetchall():
+                yield cls(*row)
+
+    @classmethod
+    def get_one(cls, **constraints):
+        """
+        Gets a specific row from the database with constraints or None.
+        """
+        try:
+            row = next(cls.get(**constraints))
+        except StopIteration:
+            return None
+
+        return row
+
+    def __str__(self):
+        return " ".join(
+            [
+                "{}:{}".format(k, v)
+                for k, v in sorted(self.field_values.items())
+            ]
+        )
+
+
+class Activity(Object):
+    """
+    Represents a daily activity in the database.
+    """
+
+    @classmethod
+    def fields(cls):
+        return {
+            "create_date": "TEXT DEFAULT CURRENT_DATE",
+            "card_id": "TEXT",
+        }
+
+    @classmethod
+    def required_fields(cls):
+        return ["create_date", "card_id", "completed"]
